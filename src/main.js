554a58d54da29bcd3136a6010eb79cc087b91302
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2024-08-24 14:36:19 +0200

    ver.0.0.2

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+iziToast.error({
+    class: "error-alert",
+    title: "Error",
+    titleColor: "white",
+    message: "Try svg",
+    messageColor: "white",
+    position: "topRight"
+});
